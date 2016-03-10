28fdbc475a934e46852bd6e1e9fb143fdc8737bd
Roland Dordevic <roland@qloud.io>
2016-03-10 20:49:40 +0100

    feat: add random animation on the image on every scroll down; change image on scroll down

--- sketch.js
+++ sketch.js
@@ -1,14 +1,54 @@
-var weirdGif, img;
+var stickerArray = [], stickerURLArray = [], animationData;
+
+function getStickers(tag, howMany) {
+  var url = 'http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=' + tag;
+
+  for(var i=0; i<howMany; i++) {
+    var giphyStickerObject = loadJSON(url);
+    stickerArray.push(giphyStickerObject);
+  }
+}
+
 function preload() {
-  var url = 'http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=weird';
-  weirdGif = loadJSON(url);
+  getStickers('weird', 10);
+  animationData = loadJSON('animations.json');
 }
 
 function setup() {
+  noCanvas();
   noLoop();
-  img = createImg(weirdGif.data.fixed_width_small_url);
+  var img = document.createElement('img');
+  document.getElementById('GifHolder').appendChild(img);
+  
+  for(var i = 0; i < stickerArray.length; i++) {
+    var sticker = stickerArray.pop();
+    stickerURLArray.push(sticker.data.image_original_url);
+  }
+
+  img.src = stickerURLArray.pop();
+
+  hunt(document.getElementById('GifHolder'), {
+    in: function() {
+      // random animation
+      var randomNumber = random(0, animationData.animations.length);
+          randomNumber = Math.round(randomNumber);
+      var animationType = animationData.animations[randomNumber];
+      var imgClassList = this.getElementsByTagName('img')[0].classList;
+      imgClassList.add('animated');
+      imgClassList.add(animationType);
+    },
+    out: function() {
+      // remove animation
+      this.getElementsByTagName('img')[0].className = "";
+
+      // change img src   
+      img.src = stickerURLArray.pop();
+      
+    },
+    persist: true
+  });  
 }
 
 function draw() {
-  console.log(weirdGif.data.fixed_width_small_url);
+
 }
